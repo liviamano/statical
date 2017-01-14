d2552a088c19999b7191abe2cb9d0779b209e8c7
Liv M <manolivia95@gmail.com>
2017-01-14 18:15:02 +0100

    Kendo testing

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
 
-$query = require 'factory.php';
+//$query = require 'factory.php';
+//
+//$persons = $query->selectAll('person');
+//
+//var_dump($persons);
 
-$persons = $query->selectAll('person');
+//require 'index.view.php';
 
-var_dump($persons);
+//class Data
+//{
+//    public $name;
+//    public $age;
+//}
+
+//$data = new Data();
+//$data->name = 'Livia';
+//$data->age = 21;
+
+$data = [
+    array('name'=>'Livia', 'age'=> '21'),
+    array('name'=>'Alisa', 'age'=> '18'),
+    array('name'=>'Andrea', 'age'=> '16')
+];
+
+echo json_encode($data);
 
 //require 'index.view.php';
