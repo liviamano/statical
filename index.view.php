693a464e61609627c3ae84b6f3a4b98af5d23fb4
Liv M <manolivia95@gmail.com>
2017-01-15 14:24:28 +0100

    Initial W3CSS implementation

--- index.view.php
+++ index.view.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="static/css/kendo/kendo.black.min.css">
     <link rel="stylesheet" href="static/css/kendo/kendo.common.core.min.css">
     <link rel="stylesheet" href="static/css/kendo/kendo.bootstrap.min.css">
+    <link rel="stylesheet" href="static/css/w3.css">
 
     <script type="text/javascript" src="static/js/kendo/jquery.min.js"></script>
     <script type="text/javascript" src="static/js/kendo/kendo.all.min.js"></script>
@@ -16,6 +17,42 @@
     <script type="text/javascript" src="static/js/index.js"></script>
 </head>
 <body>
-<div id="chart" class="mychart"></div>
+
+
+<div class="w3-row w3-border">
+    <div class="w3-col m3 w3-padding-right">
+        <div class="w3-content w3-left w3-padding-16">
+            <div class="w3-center" id="about-statical">
+                <h1>Rreth Statical:</h1>
+                <p>
+                    Statical eshte nje gjenerues grafik i statistikave te CENSUSIT te mbledhura nga INSTAT
+                    pergjate viteve <i>2001-2011</i>
+                </p>
+            </div>
+            <div class="w3-center" id="about-us">
+                <h3>Rreth nesh:</h3>
+                <ul class="w3-ul w3-border">
+                    <li><a href="#">Livia Mano</a></li>
+                    <li><a href="#">Madrit Kacabumi</a></li>
+                    <li><a href="#">Margarit Zisi</a></li>
+                    <li><a href="#">Kundret Bejko</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="w3-col m9 w3-padding-left w3-border-right">
+        <header>
+            <ul class="w3-navbar w3-green">
+                <li class="w3-navitem w3-right"><a href="#">Home</a></li>
+                <li class="w3-navitem w3-right"><a href="#">Graphics</a></li>
+                <li class="w3-navitem w3-right"><a href="#">About us</a></li>
+            </ul>
+        </header>
+
+        <div id="chart"></div>
+    </div>
+</div>
+
+
 </body>
 </html>
