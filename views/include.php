051c3d374b3da7143cf3942629e04b07b58b5df1
Liv M <manolivia95@gmail.com>
2017-02-22 21:55:41 +0100

    V.0.1

--- views/include.php
+++ views/include.php
@@ -4,39 +4,39 @@
     <meta charset="UTF-8">
     <title>Document</title>
 
-    <link rel="stylesheet" href="static/css/kendo/kendo.common.min.css">
-    <link rel="stylesheet" href="static/css/kendo/kendo.black.min.css">
-    <link rel="stylesheet" href="static/css/kendo/kendo.common.core.min.css">
-    <link rel="stylesheet" href="static/css/kendo/kendo.bootstrap.min.css">
+    <link rel="stylesheet" href="../static/css/kendo/kendo.common.min.css">
+    <link rel="stylesheet" href="../static/css/kendo/kendo.black.min.css">
+    <link rel="stylesheet" href="../static/css/kendo/kendo.common.core.min.css">
+    <link rel="stylesheet" href="../static/css/kendo/kendo.bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="static/css/w3.css">
+    <link rel="stylesheet" href="../static/css/w3.css">
 
-    <script type="text/javascript" src="static/js/kendo/jquery.min.js"></script>
-    <script type="text/javascript" src="static/js/kendo/kendo.all.min.js"></script>
-    <script type="text/javascript" src="static/js/kendo/kendo.dataviz.min.js"></script>
-    <script type="text/javascript" src="static/js/kendo/kendo.web.min.js"></script>
+    <script type="text/javascript" src="../static/js/kendo/jquery.min.js"></script>
+    <script type="text/javascript" src="../static/js/kendo/kendo.all.min.js"></script>
+    <script type="text/javascript" src="../static/js/kendo/kendo.dataviz.min.js"></script>
+    <script type="text/javascript" src="../static/js/kendo/kendo.web.min.js"></script>
 </head>
 <body>
-
 <nav class="w3-sidenav w3-teal" style="width: 21%">
     <ul class="w3-ul w3-hoverable">
         <li class="w3-hover-blue">
-            <a id="home" href="index.view.php"> <i class="material-icons">home</i></a>
+            <a id="home" href="../index.view.php"> <i class="material-icons">home</i></a>
         </li>
         <li class="w3-hover-blue">
-            <a href="views/education.view.php">Arsimi</a>
+            <a href="education.view.php">Arsimi</a>
         </li>
         <li class="w3-hover-blue">
-            <a href="views/disabled.view.php">Njerezit me aftesi te kufizuar</a>
+            <a href="disabled.view.php">Njerezit me aftesi te kufizuar</a>
         </li>
         <li class="w3-hover-blue">
-            <a href="views/movement.view.php">Shperndarja e popullsise</a>
+            <a href="movement.view.php">Shperndarja e popullsise</a>
         </li>
     </ul>
     <br>
 
     <div class="w3-center" id="about-statical">
         <h3>Rreth Statical:</h3>
+
         <span>
             Statical eshte nje gjenerues grafik i statistikave te CENSUSIT te mbledhura nga INSTAT
             pergjate viteve <i>2001-2011</i>
@@ -61,14 +61,8 @@
             </li>
             <li>
                 <br>
-                <img src="static/img/logo.png" alt="universiteti i tiranes" width="50" height="50">
+                <img src="../static/img/logo.png" alt="universiteti i tiranes" width="50" height="50">
             </li>
         </ul>
     </div>
 </nav>
-<div style="margin-left: 21%">
-</div>
-
-
-</body>
-</html>
